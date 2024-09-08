ac24b97d6a0badc7582608429c84d5465de38308
Himanshu yadav <himanshuyadav@Himanshus-Laptop.local>
2024-09-08 10:25:02 +0530

    files and folders setups are done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./global.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import Home from "./_root/pages/Home";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello Social media app!</h1>;
+  return <main className="flex h-screen">
+    <Routes>
+      <Route path="/sign-in" element={<SigninForm />}/>
+
+      <Route index element={<Home />}/>
+    </Routes>
+  </main>;
 };
 
 export default App;
